bb17e512b004fb322ea7b6194c98047b983d50db
Bjarki Madsen <bjerkins8@gmail.com>
2018-04-15 13:43:40 +0200

    sorry

---
+++ src/config/firebase/index.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyA26r9OO1O2ee3W647UOq7rGvjt7TpjqYY',
+    authDomain: 'imbinn-dev.firebaseapp.com',
+    databaseURL: 'https://imbinn-dev.firebaseio.com',
+    projectId: 'imbinn-dev',
+    storageBucket: 'imbinn-dev.appspot.com',
+    messagingSenderId: '178521887826',
+};
+
+export default firebaseConfig;
